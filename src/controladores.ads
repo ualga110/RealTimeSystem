1d5dd0b3185f17a5d9042e53344844ea3b9ba79d
fca758 <fca758@inlumine.ual.es>
2026-03-19 19:52:24 +0100

    ejercicio 2 pero esta algo sopechoso

---
+++ src/controladores.ads
@@ -0,0 +1,19 @@
+with Datos; use Datos;
+
+package Controladores is
+
+   ST2_Optimo : constant Float := 78.0;
+   SD1_Optimo : constant Float := 23.0;
+
+   function Calcular_SC1 (SR1    : in Valor_Radiacion;
+                          ST4    : in Valor_Temperatura;
+                          ST1_Prev : in Float;
+                          ST2_Prev : in Float)
+                          return Valor_Caudal;
+
+   function Calcular_SC2 (ST3    : in Valor_Temperatura;
+                          ST2_Prev : in Float)
+                          return Valor_Caudal;
+
+
+end Controladores;
