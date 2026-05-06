df82e1317ccce94bcf00b9617feacff3ecd7353a
Gabriel Eduardo Auteri <ga110@inlumine.ual.es>
2026-05-06 18:29:27 +0200

    Firt step, do the procedures of main characters.

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,23 @@
 procedure Main is
 
 begin
-   --  Insert code here.
-   null;
+
+   -- Bucle infinito del procedimiento cíclico secuencial
+
+   loop
+
+      -- El orden de ejecucion viene dado por el criterio y el periodo establecidos
+
+
+      -- 1. Se ejecuta la tarea con mayor nivel de prioridad (Prevencion de daños)
+      Sistema_Seguridad;
+
+      -- 2. Se ejecuta tarea nivel de prioridad medio (Control de la energia)
+      Control_Campo_Solar;
+
+      --3. Ejecutamos la tarea de menor prioridad (Produccion)
+      Control_Modulo_MD;
+
+
+   end loop;
 end Main;
--- src/procedimientos.adb
+++ src/procedimientos.adb
@@ -1,37 +1,47 @@
-pragma Ada_2012;
+with Ada.Text_IO; Use Ada.Text_IO;
+
 package body Procedimientos is
 
-   -------------------------
-   -- Control_Campo_Solar --
-   -------------------------
+   -- Tarea de Control de Campo Solar (CCS)
 
    procedure Control_Campo_Solar is
    begin
-      pragma Compile_Time_Warning
-        (Standard.True, "Control_Campo_Solar unimplemented");
-      raise Program_Error with "Unimplemented procedure Control_Campo_Solar";
+      Put_Line("CCS - Solicitud la lectura de la tarjeta A/D....");
+      Put_Line("CCS - Lectura de sensores (ST1, ST2, ST4, SR1)...");
+      Put_Line("CCS - Calculo accion de control para caudal optimo"...);
+      Put_Line("CCS - Escritura señal de control (SC1) en Bomba 1 mediante A/D");
+      Put_Line("CCS - Enviando telemetria a la Visualizacion por Pantalla (VP)...");
+      Put_Line("CCS - Registro de los datos en el Almacenamiento de Datos (BD)...");
+      Put_Line("------------------------------------------------------------------------");
+
    end Control_Campo_Solar;
 
-   -----------------------
-   -- Sistema_Seguridad --
-   -----------------------
+   -- Tarea de Seguridad (SS)
 
    procedure Sistema_Seguridad is
    begin
-      pragma Compile_Time_Warning
-        (Standard.True, "Sistema_Seguridad unimplemented");
-      raise Program_Error with "Unimplemented procedure Sistema_Seguridad";
+      Put_Line("SS - Solicitud de lectura Tarjeta A/D....");
+      Put_Line("SS - Lectura entrada del modulo y la salida de la temperatura del campo (ST2)...");
+      Put_Line("SS - Comprobación niveles de seguridad (T < 95 C)...");
+      Put_line("SS - Enviando estado a la Visualizacion por Pantalla (VP)...");
+      Put_line("SS - Registro alarmas en el Almacenamiento de Datos (BD)....");
+      Put_Line("--------------------------------------------------------------------------------------");
+
    end Sistema_Seguridad;
 
-   -----------------------
-   -- Control_Modulo_MD --
-   -----------------------
+   -- Tarea de Control del Modulo MD (CMD)
 
    procedure Control_Modulo_MD is
    begin
-      pragma Compile_Time_Warning
-        (Standard.True, "Control_Modulo_MD unimplemented");
-      raise Program_Error with "Unimplemented procedure Control_Modulo_MD";
+      Put_Line("CMD - Solicitud de lectura Tarjeta A/D....");
+      Put_Line("CMD - Lectura de sensores de temperatura del modulo (ST2, ST3)...");
+      Put_Line("CMD - Calculo de Caudal requerido para producir 23 L/h...");
+      Put_Line("CMD - Escribiendo señal de control (SC2) en bomba 2 mediante A/D....");
+      Put_Line("CMD - Enviando telemetria a la Visualizacion por Pantalla (VP)...");
+      Put_Line("Registro de los datos en el Almacenamiento de Datos (BD)...");
+      Put_Line("--------------------------------------------------------------------------");
+
+
    end Control_Modulo_MD;
 
 end Procedimientos;
