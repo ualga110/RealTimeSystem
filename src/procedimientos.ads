b82596a2624810f353d06580b3f66be83372d489
fca758 <fca758@inlumine.ual.es>
2026-05-10 13:28:42 +0200

    pongo programas ejercicio 1 en src de practica2

---
+++ src/procedimientos.ads
@@ -0,0 +1,9 @@
+package Procedimientos is
+   
+   -- Se declaran los tres procesos establecidos en el sistema principal
+   procedure Control_Campo_Solar;
+   procedure Sistema_Seguridad;
+   procedure Control_Modulo_MD;
+   
+
+end Procedimientos;
