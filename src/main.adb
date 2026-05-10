f204a4217f6fe7a3a762a4253d7f98a150424694
fca758 <fca758@inlumine.ual.es>
2026-05-10 13:16:41 +0200

    try to fix this shit

--- src/main.adb
+++ src/main.adb
@@ -1,24 +1,6 @@
-with Procedimientos; use Procedimientos;
 procedure Main is
 
 begin
-
-   -- Bucle infinito del procedimiento cíclico secuencial
-
-   loop
-
-      -- El orden de ejecucion viene dado por el criterio y el periodo establecidos
-
-
-      -- 1. Se ejecuta la tarea con mayor nivel de prioridad (Prevencion de daños)
-      Sistema_Seguridad;
-
-      -- 2. Se ejecuta tarea nivel de prioridad medio (Control de la energia)
-      Control_Campo_Solar;
-
-      --3. Ejecutamos la tarea de menor prioridad (Produccion)
-      Control_Modulo_MD;
-
-
-   end loop;
+   --  Insert code here.
+   null;
 end Main;
